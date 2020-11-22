01193ca48cd40431b0f081ddf862a96e7615b3b7
aland mariwan <kontakt@aland-mariwan.de>
2020-11-22 12:54:07 +0100

    Todo List

---
+++ src/environments/firebase.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDaEW83qAOozjJbbJP1YYbEHxxfFksdSHQ',
+  authDomain: 'amariwan-todo-app.firebaseapp.com',
+  databaseURL: 'https://amariwan-todo-app.firebaseio.com',
+  storageBucket: 'amariwan-todo-app.appspot.com'
+};
